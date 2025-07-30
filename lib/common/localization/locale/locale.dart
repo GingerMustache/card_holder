40d0e8c8ced4282893cd47591782e59e38dafeeb
Arthur <artzaneg@gmail.com>
2025-07-30 10:33:04 +0300

    save

--- lib/common/localization/locale/locale.dart
+++ lib/common/localization/locale/locale.dart
@@ -1,10 +1,30 @@
+import 'dart:ui';
+
 import 'package:card_holder/common/localization/i18n/strings.g.dart';
 import 'package:card_holder/common/services/secure_storage.dart';
+import 'package:flutter_i18n/flutter_i18n_delegate.dart';
+import 'package:flutter_i18n/loaders/decoders/json_decode_strategy.dart';
+import 'package:flutter_i18n/loaders/file_translation_loader.dart';
 
 class LocaleClass {
   LocaleClass._();
 
-  static Future<void> initLocale() async {
+  static Future<FlutterI18nDelegate> initLocaleDelegate() async {
+    _initLocale();
+    String? langLocal = await SecureStorage().read(SecureKeys.lang.name);
+
+    return FlutterI18nDelegate(
+      translationLoader: FileTranslationLoader(
+        useCountryCode: false,
+        fallbackFile: 'en',
+        basePath: 'assets/locale',
+        decodeStrategies: [JsonDecodeStrategy()],
+        forcedLocale: langLocal == '' ? null : Locale(langLocal),
+      ),
+    );
+  }
+
+  static Future<void> _initLocale() async {
     String lc = '';
 
     lc = await SecureStorage().read(SecureKeys.lang.name);
--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,13 @@
 import 'package:card_holder/common/localization/i18n/strings.g.dart';
 import 'package:card_holder/common/localization/locale/locale.dart';
 import 'package:card_holder/common/services/di_container/di_container.dart';
-import 'package:card_holder/common/services/secure_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_i18n/flutter_i18n_delegate.dart'
-    show FlutterI18nDelegate;
-import 'package:flutter_i18n/loaders/decoders/json_decode_strategy.dart'
-    show JsonDecodeStrategy;
-import 'package:flutter_i18n/loaders/file_translation_loader.dart'
-    show FileTranslationLoader;
 
 void main() async {
-  LocaleClass.initLocale();
-  String? langLocal = await SecureStorage().read(SecureKeys.lang.name);
-
-  final FlutterI18nDelegate flutterI18nDelegate = FlutterI18nDelegate(
-    translationLoader: FileTranslationLoader(
-      useCountryCode: false,
-      fallbackFile: 'en',
-      basePath: 'assets/locale',
-      decodeStrategies: [JsonDecodeStrategy()],
-      forcedLocale: langLocal == '' ? null : Locale(langLocal),
-    ),
-  );
-  final DiContainerProvider diContainer = DiContainer();
   WidgetsFlutterBinding.ensureInitialized();
+
+  final DiContainerProvider diContainer = DiContainer();
+  final flutterI18nDelegate = await LocaleClass.initLocaleDelegate();
   final app = diContainer.makeApp(flutterI18nDelegate);
 
   runApp(TranslationProvider(child: app));
