631bffcf0804e9e3164b92535f1893793296dcb3
Arthur <artzaneg@gmail.com>
2026-04-16 15:41:45 +0300

    save, clean

--- lib/common/constants/constants.dart
+++ lib/common/constants/constants.dart
@@ -1,6 +1,3 @@
-import 'package:card_holder/common/application/app_settings.dart';
-import 'package:card_holder/common/extensions/app_extensions.dart';
 import 'package:flutter/material.dart';
 
 part 'global.dart';
-part 'snacks.dart';
